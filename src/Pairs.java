50e575459dcc051487ac9a48b4ad081b065a5dd5
user <istlud.klint@gmail.com>
2019-05-18 16:04:29 +0200

    pairs

--- src/Pairs.java
+++ src/Pairs.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Pairs {
+
+    public Map<String, String> pairs(String[] strings) {
+        Map<String, String> map = new HashMap();
+
+        for(String s : strings){
+            map.put(s.substring(0,1), s.substring(s.length()-1));
+        }
+
+        return map;
+    }
 }
