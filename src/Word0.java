338047f3455d3f6859bd04c48a7484dacb69aea3
user <istlud.klint@gmail.com>
2019-05-18 16:07:20 +0200

    word0

---
+++ src/Word0.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Word0 {
+
+    public Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> map = new HashMap();
+
+        for(String s : strings){
+            map.put(s, 0);
+        }
+
+        return map;
+    }
+
+}
