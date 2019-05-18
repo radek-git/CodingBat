626056dbd94354ef04e2e7005644c3949e076c0c
user <istlud.klint@gmail.com>
2019-05-18 16:19:56 +0200

    topping1

---
+++ src/Topping1.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+public class Topping1 {
+
+    public Map<String, String> topping1(Map<String, String> map) {
+        if(map.containsKey("ice cream")){
+            map.put("ice cream", "cherry");
+        }
+
+        map.put("bread", "butter");
+
+        return map;
+    }
+
+}
