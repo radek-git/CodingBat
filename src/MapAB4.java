a497913a85b1b0ffa0aa809c49566bb01629cbad
user <istlud.klint@gmail.com>
2019-05-19 14:48:31 +0200

    wordAppend

--- src/MapAB4.java
+++ src/MapAB4.java
@@ -2,6 +2,8 @@ import java.util.Map;
 
 public class MapAB4 {
 
+
+
     public Map<String, String> mapAB4(Map<String, String> map) {
 
         if(!map.containsKey("a") || !map.containsKey("b")){ return map; }
